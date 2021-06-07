3f39186627ab0388bec16ce30512018453e076ab
Ajith Ramachandran <ajithar204@gmail.com>
2021-06-08 01:07:34 +0530

    Initial implementation of math functions.
    Implemented functions from C <math.h> library.

--- yammpy/include/yammpy.h
+++ yammpy/include/yammpy.h
@@ -1,4 +1,67 @@
 #ifndef YAMMPY_H
 #define YAMMPY_H
 
+#include <Python.h>
+
+/* constants */
+
+#ifndef PI
+#define PI 3.1415926535897932384626433832795029L
+#endif
+
+#ifndef E
+#define E 2.7182818284590452353602874713526625L
+#endif
+
+#ifndef TAU
+#define TAU 6.2831853071795864769252867665590057683943L
+#endif
+
+#ifndef PHI
+#define PHI 1.61803398874989484820L
+#endif
+
+#ifndef WAL
+#define WAL 2.09455148154232659148L
+#endif
+
+/*
+function wrapper for <math.h> functions
+*/
+
+static PyObject* yammpy_func(PyObject* , double (*)(double));
+
+#define FUNC(funcname, func, docstring)                                     \
+    static PyObject* yammpy_##funcname(PyObject* self, PyObject* args) {    \
+        return yammpy_func(args, func);                                     \
+    }                                                                       \
+    PyDoc_STRVAR(yammpy_##funcname##_doc, docstring);
+
+
+/*
+wrap functions from <math.h> library 
+*/
+
+FUNC(acos, acos, "Returns the arc cosine of x (arccos(x)) in the range [0 ; π].")
+FUNC(acosh, acosh, "Returns the inverse hyperbolic cosine of arg (cosh^-1(arg), or arcosh(arg)) on the interval [0, +∞].")
+FUNC(asin, asin, "Returns the arc sine of x (arcsin(x)) in the range [-(π/2) ; +(π/2).")
+FUNC(asinh, asinh, "Returns the inverse hyperbolic sine of arg (sinh^-1(arg), or arsinh(arg).")
+FUNC(atan, atan, "Returns the arc tangent of x (arctan(x)) in the range [-(π/2) ; +(π/2)].")
+FUNC(atanh, atanh, "Returns the inverse hyperbolic tangent of arg (tanh^-1(arg), or artanh(arg)).")
+FUNC(cos, cos, "Returns the sine of x (cos(x)) in the range [-1 ; +1].")
+FUNC(cosh, cosh, "Returns the hyperbolic cosine of arg (cosh(arg), or (((e^arg)+(e^-arg))/2).")
+FUNC(erf, erf, "Returns value of the error function of x.")
+FUNC(erfc, erfc, "Returns value of the complementary error function of x.")
+FUNC(exp, exp, "Returns  the base-e exponential of arg (e^arg).")
+FUNC(expm1, expm1, "Returns e^arg-1.")
+FUNC(fabs, fabs, "Returns the absolute value of floating point value x.")
+FUNC(gamma, tgamma, "Returns the value of the gamma function of x.")
+FUNC(lgamma, lgamma, "Returns the value of the logarithm of the gamma function of x.")
+FUNC(log1p, log1p, "Returns ln(1+arg)")
+FUNC(sin, sin, "Returns the sine of x (sin(x)) in the range [-1 ; +1].")
+FUNC(sinh, sinh, "Returns the hyperbolic sine of x (sinh(x), or (((e^arg)-(e^-arg))/2)")
+FUNC(sqrt, sqrt, "Returns square root of x.")
+FUNC(tan, tan, "Returns the sine of x (tan(x)).")
+FUNC(tanh, tanh, "Returns the hyperbolic tangent of arg (tanh(arg), or (((e^arg)-(e^-arg))/((e^arg)+(e^-arg)))")
+
 #endif // YAMMPY_H
--- yammpy/yammpy.c
+++ yammpy/yammpy.c
@@ -4,19 +4,78 @@
 
 #include "include/yammpy.h"
 
+/* yammpy_func is used to wrap a libm function f that takes a double argument and returns a double. */
+
+static PyObject *
+yammpy_func(PyObject* args, double (*func)(double)) {
+    double x, ret;
+
+    x = PyFloat_AsDouble(args);
+
+    if (x == -1.0 && PyErr_Occurred())
+        return NULL;
+
+    ret = (*func)(x);
+
+    if (Py_IS_NAN(ret) && !Py_IS_NAN(x)) {
+        PyErr_SetString(PyExc_ValueError, "domain error"); /* invalid arg */
+        return NULL;
+    }
+    
+    return PyFloat_FromDouble(ret);
+}
+
 static PyMethodDef yammpy_methods[] = {
+    {"acos",            yammpy_acos,      METH_O,         yammpy_acos_doc},
+    {"acosh",           yammpy_acosh,     METH_O,         yammpy_acosh_doc},
+    {"asin",            yammpy_asin,      METH_O,         yammpy_asin_doc},
+    {"asinh",           yammpy_asinh,     METH_O,         yammpy_asinh_doc},
+    {"atan",            yammpy_atan,      METH_O,         yammpy_atan_doc},
+    {"atanh",           yammpy_atanh,     METH_O,         yammpy_atanh_doc},
+    {"cos",             yammpy_cos,       METH_O,         yammpy_cos_doc},
+    {"cosh",            yammpy_cosh,      METH_O,         yammpy_cosh_doc},
+    {"erf",             yammpy_erf,       METH_O,         yammpy_erf_doc},
+    {"erfc",            yammpy_erfc,      METH_O,         yammpy_erfc_doc},
+    {"exp",             yammpy_exp,       METH_O,         yammpy_exp_doc},
+    {"expm1",           yammpy_expm1,     METH_O,         yammpy_expm1_doc},
+	{"fabs",            yammpy_fabs,      METH_O,         yammpy_fabs_doc},
+    {"gamma",           yammpy_gamma,     METH_O,         yammpy_gamma_doc},
+    {"lgamma",          yammpy_lgamma,    METH_O,         yammpy_lgamma_doc},
+    {"log1p",           yammpy_log1p,     METH_O,         yammpy_log1p_doc},
+    {"sin",             yammpy_sin,       METH_O,         yammpy_sin_doc},
+    {"sinh",            yammpy_sinh,      METH_O,         yammpy_sinh_doc},
+    {"sqrt",            yammpy_sqrt,      METH_O,         yammpy_sqrt_doc},
+    {"tan",             yammpy_tan,       METH_O,         yammpy_tan_doc},
+    {"tanh",            yammpy_tanh,      METH_O,         yammpy_tanh_doc},
     {NULL}, /* sentinel */
 };
 
+PyDoc_STRVAR(yammpy_doc, "yammpy provides access to the mathematical functions");
+
 static struct PyModuleDef yammpy = {
-        PyModuleDef_HEAD_INIT,
-        "yammpy",
-        "Yet Another Math Module for Python",
-        -1,
-        yammpy_methods,
+    PyModuleDef_HEAD_INIT,
+    "yammpy",
+    yammpy_doc,
+    -1,
+    yammpy_methods,
 };
 
-PyMODINIT_FUNC 
+PyMODINIT_FUNC
 PyInit_yammpy(void) {
-    return PyModule_Create(&yammpy);
+	PyObject *yamm;
+
+	yamm = PyModule_Create(&yammpy);
+
+	if(yamm == NULL)
+		return NULL;
+	
+	PyModule_AddObject(yamm, "pi", PyFloat_FromDouble(PI));
+    PyModule_AddObject(yamm, "e", PyFloat_FromDouble(E));
+    PyModule_AddObject(yamm, "tau", PyFloat_FromDouble(TAU));
+	PyModule_AddObject(yamm, "phi", PyFloat_FromDouble(PHI));
+    PyModule_AddObject(yamm, "wal", PyFloat_FromDouble(WAL));
+	PyModule_AddObject(yamm, "inf", PyFloat_FromDouble(INFINITY));
+    PyModule_AddObject(yamm, "nan", PyFloat_FromDouble(NAN));
+
+    return yamm;
 }
